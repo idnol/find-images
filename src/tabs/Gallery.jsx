b51bf0a9e4da3e64c63f81c362e9593dc3b4bf30
Olesia511 <131859369+Olesia511@users.noreply.github.com>
2023-11-18 12:18:07 +0200

    task 1

--- src/tabs/Gallery.jsx
+++ src/tabs/Gallery.jsx
@@ -1,6 +1,6 @@
 import { Component } from 'react';
 
-import {getImages} from 'service/image-service';
+import { getImages } from 'service/image-service';
 import { Button, SearchForm, Grid, GridItem, Text, CardItem } from 'components';
 
 export class Gallery extends Component {
@@ -8,33 +8,68 @@ export class Gallery extends Component {
     val: '',
     page: 1,
     images: [],
-    total: 0
-  }
+    total: 0,
+    error: null,
+  };
 
   componentDidUpdate(prevProps, prevState) {
-    if (prevState.val !== this.state.val || prevState.page !== this.state.page) {
+    if (
+      prevState.val !== this.state.val ||
+      prevState.page !== this.state.page
+    ) {
       this.fetchImages(this.state.val, this.state.page);
     }
   }
 
   fetchImages = async (query, page) => {
     try {
-      const data = await getImages(query, page);
-      console.log(data);
-    } catch (error) {
-      console.log(error);
-    }
-  }
+      const { photos, total_results } = await getImages(query, page);
 
-  handleSubmit = (e) => {
-    this.setState({ val: e });
-  }
+      this.setState(prevState => ({
+        images: [...prevState.images, ...photos],
+        total: total_results,
+      }));
+    } catch (error) {
+      this.setState({ error: error.message });
+    }
+  };
+
+  handleSubmit = e => {
+    this.setState({
+      val: e,
+      page: 1,
+      images: [],
+      total: 0,
+      error: null,
+    });
+  };
+
+  btnClickLoadMore = () => {
+    this.setState(prev => ({ page: prev.page + 1 }));
+  };
+
   render() {
-    console.log(this.state.val)
+    const { images, total, error, val } = this.state;
+    console.log(this.state.val);
     return (
       <>
         <SearchForm onSubmit={this.handleSubmit} />
-        {/* <Text textAlign="center">Sorry. There are no images ... 😭</Text> */}
+        {error && <Text textAlign="center">Sorry {error}😭</Text>}
+        {images.length === 0 && val !== '' && (
+          <Text textAlign="center">Sorry. Bad request {val} 😭</Text>
+        )}
+        <Grid>
+          {images.map(({ src: { large }, alt, avg_color, id }) => (
+            <GridItem key={id}>
+              <CardItem color={avg_color}>
+                <img src={large} alt={alt} />
+              </CardItem>
+            </GridItem>
+          ))}
+        </Grid>
+        {total > images.length && (
+          <Button onClick={this.btnClickLoadMore}> Load more</Button>
+        )}
       </>
     );
   }
