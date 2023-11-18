7e96b62a54ba1a3ff9890047110b0a7f89ba9385
Dmytro Shkatula <idnol84@gmail.com>
2023-11-18 11:36:31 +0200

    start

--- src/service/image-service.js
+++ src/service/image-service.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_KEY = '';
+const API_KEY = 'kAKLn9CKYFL3uiTIZxHJ5z4gDNTzM73yMCBup8iVHkW576WETT0mVpCV';
 axios.defaults.baseURL = 'https://api.pexels.com/v1/';
 axios.defaults.headers.common['Authorization'] = API_KEY;
 axios.defaults.params = {
@@ -8,4 +8,7 @@ axios.defaults.params = {
   per_page: 15,
 };
 
-export const getImages = (query, page) => {};
+export const getImages = async (query, page) => { 
+  const {data} = await axios.get(`search?query=${query}&page=${page}`);
+  return data;
+ };
